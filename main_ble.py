732d5dc6c887fa0150db7d3b2a26c6e979f0f08d
quim81 <quim81@gmail.com>
2023-01-17 14:50:10 +0100

    Add Bleak

--- main_ble.py
+++ main_ble.py
@@ -1,10 +1,22 @@
 import asyncio
+
 from bleak import BleakScanner
 
+
 async def main():
+    # cube_found = false
     devices = await BleakScanner.discover()
+    mubik: BLEdevice
     for d in devices:
-        print(d.details)
+        if d.name == 'Gi144467':
+            mubik = d
+            print(mubik)
+
+    """
+    if (cube_found == true):
+        # Connect Cube
+        async with BleakClient(mubik.address) as client
+    """
 
 if __name__ == '__main__':
     asyncio.run(main())
